5d4e2d02a0e62e7b4bade2572ffcb8dcf332f713
Scott Petersen <scott@digression.org>
2016-05-15 14:15:33 -0700

    Colors, lighting.

--- Assets/SolarSystem.cs
+++ Assets/SolarSystem.cs
@@ -41,15 +41,15 @@ public class SolarSystem : MonoBehaviour {
 
     private static readonly PlanetDefinition[] planetDefinitions =
     {
-        new PlanetDefinition("Mercury", 0.5f * 0.5f, RotateDirection.Clockwise, 1, 0.24f, RotateDirection.Clockwise, 25f, Color.red),
-        new PlanetDefinition("Venus", 0.5f * 0.95f, RotateDirection.Clockwise, 2, 0.62f, RotateDirection.Clockwise, 25f, Color.red),
-        new PlanetDefinition("Earth", 0.5f, RotateDirection.Clockwise, 3, 1, RotateDirection.Clockwise, 25f, Color.blue),
-        new PlanetDefinition("Mars", 0.5f * 0.7f, RotateDirection.Clockwise, 4, 1.88f, RotateDirection.Clockwise, 25f, Color.red),
-        new PlanetDefinition("Jupiter", 1f, RotateDirection.Clockwise, 5, 11.86f, RotateDirection.Clockwise, 25f, Color.red),
-        new PlanetDefinition("Saturn", 0.9f, RotateDirection.Clockwise, 6, 29.46f, RotateDirection.Clockwise, 25f, Color.blue),
-        new PlanetDefinition("Uranus", 0.8f, RotateDirection.Clockwise, 7, 84.01f, RotateDirection.Clockwise, 25f, Color.blue),
-        new PlanetDefinition("Neptune", 0.7f, RotateDirection.Clockwise, 8, 164.8f, RotateDirection.Clockwise, 25f, Color.blue),
-        new PlanetDefinition("Pluto", 0.5f * 0.4f, RotateDirection.Clockwise, 9, 247.7f, RotateDirection.Clockwise, 25f, Color.gray),
+        new PlanetDefinition("Mercury", 0.5f * 0.5f, RotateDirection.Clockwise, 1, 0.24f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0xf0, (float)0xff / 0xc4, (float)0xff / 0x67)),
+        new PlanetDefinition("Venus", 0.5f * 0.95f, RotateDirection.Clockwise, 2, 0.62f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0x2f, (float)0xff / 0xd5, (float)0xff / 0x96)),
+        new PlanetDefinition("Earth", 0.5f, RotateDirection.Clockwise, 3, 1, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0x76, (float)0xff / 0x86, (float)0xff / 0xcc)),
+        new PlanetDefinition("Mars", 0.5f * 0.7f, RotateDirection.Clockwise, 4, 1.88f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0xcc, (float)0xff / 0x86, (float)0xff / 0x6a)),
+        new PlanetDefinition("Jupiter", 1f, RotateDirection.Clockwise, 5, 11.86f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0x8d, (float)0xff / 0xae, (float)0xff / 0x8f)),
+        new PlanetDefinition("Saturn", 0.9f, RotateDirection.Clockwise, 6, 29.46f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0xe0, (float)0xff / 0xcc, (float)0xff / 0x9f)),
+        new PlanetDefinition("Uranus", 0.8f, RotateDirection.Clockwise, 7, 84.01f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0x9f, (float)0xff / 0xd6, (float)0xff / 0xe0)),
+        new PlanetDefinition("Neptune", 0.7f, RotateDirection.Clockwise, 8, 164.8f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0x42, (float)0xff / 0xb6, (float)0xff / 0xf5)),
+        new PlanetDefinition("Pluto", 0.5f * 0.4f, RotateDirection.Clockwise, 9, 247.7f, RotateDirection.Clockwise, 25f, new Color((float)0xff / 0xad, (float)0xff / 0xad, (float)0xff / 0xad)),
     };
 
     // Use this for initialization
